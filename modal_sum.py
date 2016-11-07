1ab6a7c643148af854ec1fb15efb56ec45f49374
njlindsey <nathaniel.j.lindsey@gmail.com>
2016-11-06 16:53:50 -0800

    worked on mode summation code, need to find k for where B21 minimizes

--- modal_sum.py
+++ modal_sum.py
@@ -1,21 +1,29 @@
 import numpy as np
 
 def calc_nu(k, w, beta):
-    nu = ( k**2 - (w**2/beta**2) )**0.5
+    nu = np.sqrt( (k+0j)**2 - (w/beta)**2 )
     return nu
 
-def tl(nu, h):
-    tl = np.cosh(nu*h)
-    return tl
+def P_tl(nu, h):
+    return np.cosh(nu*h)
 
-def tr(nu, h, mu):
-    tr = np.sinh( (nu*h)/(nu*mu) )
-    return tr
+def P_tr(nu, h, mu):
+    return np.sinh( (nu*h)/(nu*mu) )
 
-def bl(nu, h, mu):
-    bl = (nu*mu)*np.sinh(nu*h)
-    return bl
+def P_bl(nu, h, mu):
+    return (nu*mu)*np.sinh(nu*h)
 
-def br(nu, h):
-    br = np.cosh(nu*h)
-    return br
+def P_br(nu, h):
+    return np.cosh(nu*h)
+
+def F_bl(nu, h, mu):
+    return (1/(2*nu*mu))*nu*mu*np.exp(-1*nu*h)
+
+def F_tr(nu, h, mu):
+    return (1/(2*nu*mu))*-1*np.exp(nu*h)
+
+def F_br(nu, h,mu):
+    return (1/(2*nu*mu))*np.exp(-1*nu*h)
+
+def F_tl(nu, h, mu):
+    return (1/(2*nu*mu))*nu*mu*np.exp(nu*h)
